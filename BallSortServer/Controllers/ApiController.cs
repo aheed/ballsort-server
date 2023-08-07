54eea4a4234e04ede3c8d77a57cec9483e2d9fc7
Anders Heed <aheed88888@gmail.com>
2023-08-07 15:05:16 +0200

    add state API

---
+++ BallSortServer/Controllers/ApiController.cs
@@ -0,0 +1,27 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using BallSortServer.Models;
+
+namespace BallSortServer.Controllers;
+
+public class ApiController : Controller
+{
+    private readonly ILogger<ApiController> _logger;
+
+    public ApiController(ILogger<ApiController> logger)
+    {
+        _logger = logger;
+    }
+
+    public string Get() {
+        return "Welcome To BallSort Web API";
+    }
+
+    [HttpPost]
+    public IActionResult Update([FromBody] BallSortStateModel newState)
+    {    
+        _logger.LogInformation("Got an update request: {cols}", newState.NofCols);
+        //return Json("Got it!");
+        return Json(newState);
+    }
+}
---
+++ BallSortServer/Models/BallSortStateModel.cs
@@ -0,0 +1,10 @@
+namespace BallSortServer.Models;
+
+public class BallSortStateModel
+{
+    public int NofRows { get; set; }
+    public int NofCols { get; set; }
+    
+    public int PosX { get; set; }
+    public int PosY { get; set; }
+}
