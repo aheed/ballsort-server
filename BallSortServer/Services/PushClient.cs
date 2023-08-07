902f3143cf07f1d192ccecba69151c287dc58668
Anders Heed <aheed88888@gmail.com>
2023-08-07 18:29:50 +0200

    add PushClient skeleton

---
+++ BallSortServer/Services/PushClient.cs
@@ -0,0 +1,13 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using BallSortServer.Models;
+
+namespace BallSortServer.Services;
+
+public class PushClient : IPushClient
+{
+    public void UpdateState(BallSortStateModel newState)
+    {
+        Console.WriteLine("PushClient.UpdateState");
+    }    
+}
